9cbc495bedf8af14864ce28ce52b3d6c9c9afba1
Tim Kellogg <student@epicodus.com>
2015-09-24 11:33:45 -0700

    add comments and post relationship

--- app/models/comment.js
+++ app/models/comment.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
-  post: DS.belongsTo('post'),
+  post: DS.belongsTo('post', {asyn: true}),
   body: DS.attr(),
   date: DS.attr()
 });
